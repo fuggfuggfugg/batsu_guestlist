b815377895a35a4d3b07a18fa2f1fcd2188ab8f4
fuggfuggfugg <pwskhisty1234>
2015-10-18 15:17:54 -0400

    logic for dropdown to load users based on selection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,12 @@
 $(function(){
 
+	// load snapshots
+	artists_by_batsu_snapshot = null;
+	artists_lookup_snapshot = null;
+	artists_by_batsu_snapshot = Defiant.getSnapshot(artists_by_batsu);
+	artists_lookup_snapshot = Defiant.getSnapshot(artists_lookup);
+	
+
 	Handlebars.registerHelper("markdownToHtml", function(text) {
 		return markdown.toHTML(text);		
 	});
@@ -10,20 +17,48 @@ $(function(){
 		btn =  $("#js-game-selection-btn");
 		loadSelectionOption = btn.attr("data-selection");
 
-
-
 		if (loadSelectionOption == "all") {
 			var source   = $("#user-template").html();
 			var template = Handlebars.compile(source);
+			$("#artist-ul .artist-li").remove();
 			$("#artist-ul").append(template(artists_lookup));
 			$("#artist-li-title").html(btn.text())
 			$("#artist-count").html(artists_lookup.artist_lookup.length + " Guests")
 			
 		}else{
-			console.log (artists_by_batsu);
-			$("#artist-li-title").html(btn.text())
-			$("#artist-count").html(artists_lookup.artist_lookup.length  + " Guests")
 
+			// searching on snapshot created with web worker
+			var found = JSON.search(artists_by_batsu_snapshot,"//"+loadSelectionOption);
+			if (found.length > 0 && found[0].actor_list.length > 0) {
+				found_list = found[0].actor_list;
+
+				// JSON.search(artists_lookup_snapshot, "//*[artist_id='Egashira' or artist_id='Agnes_Chan']")
+				var searchStr = "//*[";
+				for (var i = 0; i <= found_list.length - 1; i++) {
+					if (i==0) {
+						searchStr += "artist_id='" + found_list[i] + "'";
+					}else{
+						searchStr += " or artist_id='" + found_list[i] + "'";
+					}
+				};
+
+				searchStr += "]";
+				guestList = JSON.search(artists_lookup_snapshot, searchStr) ;
+				var guests =  {
+  					"artist_lookup": guestList
+  				}
+
+				var source   = $("#user-template").html();
+				var template = Handlebars.compile(source);
+				$("#artist-ul .artist-li").remove();
+				$("#artist-ul").append(template(guests));
+				$("#artist-li-title").html(btn.text())
+				$("#artist-count").html(guestList.length + " Guests")
+
+				
+			}else{
+				alert("list is empty")
+			}
 		}
 	}
 
